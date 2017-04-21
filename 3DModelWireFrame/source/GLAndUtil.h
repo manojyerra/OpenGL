1a0c5325c689854b1a4e68489e1f69af6acd4bbf
Manoj Yerra <manojyerra@gmail.com>
2017-04-21 15:28:25 +0530

    Added 3DModelWireFrame drawing

---
+++ 3DModelWireFrame/source/GLAndUtil.h
@@ -0,0 +1,64 @@
+#ifndef GLAndUtil_H
+#define GLAndUtil_H
+#pragma once
+
+#include <windows.h>
+#include <gl/gl.h>
+
+#define GLAndUtil util
+
+class GLAndUtil
+{
+private:
+	static float _viewX;
+	static float _viewY;
+	static float _viewW;
+	static float _viewH;
+
+	static float _left;
+	static float _right;
+	static float _bottom;
+	static float _top;
+	static float _zNear;
+	static float _zFar;
+
+	static float _zNearPlaneHalfW;
+
+	static float _angleX;
+	static float _angleY;
+	static float _angleZ;
+
+	static float _transZ;
+	static float _transX;
+	static float _transY;
+
+	static float _clearR;
+	static float _clearG;
+	static float _clearB;
+	static float _clearA;
+
+	static float SW;
+	static float SH;
+
+	static void SetFrustum(float left, float right, float bottom, float top, float zNear, float zFar);
+
+public:
+	static void Init(int screenW, int screenH);
+
+	static void SetViewport(float x, float y, float w, float h);
+	static void Begin3DDraw();
+	static void SetModelViewMatrix();
+	static void Begin2DDraw();
+	static bool UpdateCamera();
+
+	static void Clear();
+	static void ClearColor(float r, float g, float b, float a);
+	static void Get2DPosOnScreenFrom3DPos(float* pos3D, float* pos2D);
+
+	static void GLEnable(unsigned int* arr, int size, bool enable);
+	static bool GLEnable(unsigned int state, bool enable);
+	static GLfloat GLLineWidth(GLfloat width);
+	static unsigned int GLColor(unsigned int color);
+};
+
+#endif
