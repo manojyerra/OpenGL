9741c7814bfdd18580bc0d43f71ed476cf71b0ed
Manoj Yerra <manojyerra@gmail.com>
2017-06-21 23:39:54 +0530

    Removed Box2DEditor old version.

--- Box2DEditor/source/CommonLib/Cam.cpp
+++
@@ -1,5 +0,0 @@
-#include "Cam.h"
-
-float Cam::X = 0;
-float Cam::Y = 0;
-float Cam::Z = 0;
--- Box2DEditor/source/MathLib/Direction.h
+++
@@ -1,9 +0,0 @@
-class Direction
-{
-public:
-	static const int NONE = 0;
-	static const int LEFT = 1;
-	static const int RIGHT = 2;
-	static const int UP = 3;
-	static const int DOWN = 4;
-};
