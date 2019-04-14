480db31d4f9b8b61f08c45f7a49566ec8925c1f0
manojyerra <manojyerra@gmail.com>
2019-04-14 17:24:18 +0530

    Modified index.html and main.js files.

--- WebGL/NodeJS/public/main.js
+++ WebGL/NodeJS/public/main.js
@@ -34,6 +34,44 @@ async function loadTextResource(url) {
 	return text;
 }
 
+	function CompileShader(gl, shaderFileData, shaderType)
+	{
+		var shaderObject =  gl.createShader(shaderType);
+		gl.shaderSource(shaderObject, shaderFileData);
+		gl.compileShader(shaderObject);
+
+		if (!gl.getShaderParameter(shaderObject, gl.COMPILE_STATUS))
+		{
+			console.error('ERROR compiling shader : ', gl.getShaderInfoLog(shaderObject));
+			return;
+		}
+		else if(gl.getShaderInfoLog(shaderObject))
+		{
+			console.error('Shader Compilation Log : ', gl.getShaderInfoLog(shaderObject));
+		}
+		
+		return shaderObject;
+	}
+
+	function LinkShader(gl, programID, vertexShaderObj, fragmentShaderObj)
+	{
+		gl.attachShader(programID, vertexShaderObj);
+		gl.attachShader(programID, fragmentShaderObj);
+		gl.linkProgram(programID);
+		
+		if(!gl.getProgramParameter(programID, gl.LINK_STATUS))
+		{
+			console.error('ERROR linking shader : ', gl.getProgramInfoLog(programID));
+			return;
+		}
+		else if(gl.getProgramInfoLog(programID))
+		{
+			console.error('Shader Linking Log : ', gl.getProgramInfoLog(programID));		
+		}
+			
+		gl.detachShader(programID, vertexShaderObj);
+		gl.detachShader(programID, fragmentShaderObj);
+	}
 
 function createGLBuffer(gl, arr)
 {
